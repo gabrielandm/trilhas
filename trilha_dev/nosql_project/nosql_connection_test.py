36f9dacaf726433b5a81f7cf242445902c8a7beb
gabrielpaoli-dev <gabriel.paoli@klumulus.com>
2021-02-11 13:52:49 -0300

    New changes made and new mini project added

---
+++ trilha_dev/nosql_project/nosql_connection_test.py
@@ -0,0 +1,6 @@
+"""
+Connection string:
+    client = pymongo.MongoClient("mongodb+srv://login:<password>@cluster0.iaj61.mongodb.net/<dbname>?retryWrites=true&w=majority")
+    db = client.test
+"""
+
